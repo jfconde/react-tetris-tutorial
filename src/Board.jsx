fe1f6856c4700dd48d0e80cc6e929bd27876e0f4
juancarlos.fernandez.conde.next <juancarlos.fernandez.conde.next@bbva.com>
2019-05-16 21:13:42 +0200

    feat(board): main logic without the fall timer

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import Row from './Row';
 import Block from './Block';
-import {getRandomTetromino} from "./util";
+import {
+    getRandomTetromino,
+    rotateTetrominoMatrix,
+    tetrominoMatrixCollides,
+    mergeBoardAndTetromino,
+    tetrominoMatrixOutOfBounds
+} from "./util";
 
 const BOARD_WIDTH = 10;
+const BOARD_HEIGHT = 20;
 
-const initialBoard = new Array(20).fill(new Array(10).fill(null));
+const initialBoard = new Array(BOARD_HEIGHT).fill(null).map(i => new Array(BOARD_WIDTH).fill(null));
+
 const getNewTetromino = () => ({
     x     : (BOARD_WIDTH / 2) - 2,
     y     : -1,
@@ -24,8 +32,86 @@ const Board = () => {
 
     useEffect(() => currentBoardElement && currentBoardElement.focus(), []);
 
+    const attemptToRotateTetromino = (n) => {
+        let matrix = [...currentTetromino.matrix];
+        for (let i = 0; i < n; i++) {
+            matrix = rotateTetrominoMatrix(matrix);
+        }
+        const tetromino = {...currentTetromino, matrix};
+        const boundsObj = tetrominoMatrixOutOfBounds (tetromino, BOARD_WIDTH, BOARD_HEIGHT);
+        !tetrominoMatrixCollides(board, tetromino) && !boundsObj.left && !boundsObj.right && !boundsObj.bottom && setCurrentTetromino(tetromino);
+    };
+
+    const attemptToMoveDownTetromino = (toBottom) => {
+        let tetromino = {...currentTetromino, y: currentTetromino.y + 1};
+        if (toBottom) {
+            while (
+                !tetrominoMatrixOutOfBounds(tetromino, BOARD_WIDTH, BOARD_HEIGHT).bottom &&
+                !tetrominoMatrixCollides(board, tetromino)
+                ) {
+                tetromino = {...tetromino, y: tetromino.y + 1}
+            }
+            tetromino.y -= 1;
+            setBoard(mergeBoardAndTetromino(board, tetromino));
+            setCurrentTetromino(getNewTetromino());
+            return false;
+        }
+
+        if (tetrominoMatrixOutOfBounds(tetromino, BOARD_WIDTH, BOARD_HEIGHT).bottom
+            || tetrominoMatrixCollides(board, tetromino)
+        ) {
+            setBoard(mergeBoardAndTetromino(board, currentTetromino));
+            setCurrentTetromino(getNewTetromino());
+            return false;
+        } else {
+            setCurrentTetromino(tetromino);
+            return true;
+        }
+    };
+
+    const attemptToMoveTetromino = (right = true) => {
+        const tetromino = {...currentTetromino, x: currentTetromino.x + (right ? 1 : -1)};
+        const boundsViolation = tetrominoMatrixOutOfBounds(tetromino, BOARD_WIDTH, BOARD_HEIGHT);
+
+        if (!boundsViolation.left && !boundsViolation.right && !tetrominoMatrixCollides(board, tetromino)) {
+            setCurrentTetromino(tetromino);
+            return true;
+        }
+        return false;
+    };
+
+    const onKeyDownHandler = (event) => {
+        switch (event.key) {
+            case 's':
+            case 'ArrowDown':
+                attemptToMoveDownTetromino();
+                break;
+            case 'a':
+            case 'ArrowLeft':
+                attemptToMoveTetromino(false);
+                break;
+            case 'd':
+            case 'ArrowRight':
+                attemptToMoveTetromino(true);
+                break;
+            case 'j':
+                attemptToRotateTetromino(3);
+                break;
+            case 'k':
+                attemptToRotateTetromino(1);
+                break;
+            case ' ':
+                attemptToMoveDownTetromino(true);
+                break;
+        }
+    };
+
     return (
-        <div className="tetris-board" tabIndex="0" ref={el => currentBoardElement = el}>
+        <div className="tetris-board"
+             tabIndex="0"
+             ref={el => currentBoardElement = el}
+             onKeyDown={onKeyDownHandler}
+        >
             {
                 virtualBoard.map(
                     (row, i) => (
--- src/util.js
+++ src/util.js
@@ -44,4 +44,35 @@ const tetrominos = [
 ];
 
 export const getRandomTetromino = () => tetrominos[Math.round(Math.random() * (tetrominos.length - 1))];
+export const rotateTetrominoMatrix = matrix => matrix.map((row, y) => row.map((col, x) => matrix[3 - x][y]));
+export const tetrominoMatrixCollides = (board, tetromino) => {
+    return tetromino.matrix.some((row, y) =>
+        row.some((block, x) =>
+            tetromino.matrix[y][x] && board[y + tetromino.y] && board[y + tetromino.y][x + tetromino.x])
+    );
+};
+export const mergeBoardAndTetromino = (board, tetromino) => board
+    .map((row, y) =>
+        row.map((block, x) =>
+            (tetromino.matrix[y - tetromino.y] && tetromino.matrix[y - tetromino.y][x - tetromino.x]) ||
+            block
+        )
+    );
 
+const mergeBoundsObject = (o1, o2) => ({
+    top   : o1.top || o2.top,
+    left  : o1.left || o2.left,
+    bottom: o1.bottom || o2.bottom,
+    right : o1.right || o2.right
+});
+
+export const tetrominoMatrixOutOfBounds = (tetromino, width, height) =>
+    tetromino.matrix.reduce(
+        (res, row, y) => mergeBoundsObject(res, row.reduce((res2, block, x) => ({
+            top   : res2.top || (block && y + tetromino.y < 0),
+            left  : res2.left || (block && x + tetromino.x < 0),
+            right : res2.right || (block && x + tetromino.x >= width),
+            bottom: res2.bottom || (block && y + tetromino.y >= height)
+        }), {})),
+        {}
+    );
