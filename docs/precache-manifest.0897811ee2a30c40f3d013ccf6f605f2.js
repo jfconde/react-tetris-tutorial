12d39bb216ccecee2362fac0e64493f2b6a66bd8
EnderFX <juanoviedo@gmail.com>
2019-05-17 14:40:29 +0200

    feat(githubDocs): code!

---
+++ docs/precache-manifest.0897811ee2a30c40f3d013ccf6f605f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c26bc49c238148c9cf5c6a368158652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d615eb2df7d882493a2",
+    "url": "/static/css/main.007beecf.chunk.css"
+  },
+  {
+    "revision": "b20e5a46a7fb8f51c8f9",
+    "url": "/static/js/2.aff167c4.chunk.js"
+  },
+  {
+    "revision": "3d615eb2df7d882493a2",
+    "url": "/static/js/main.19f275b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
